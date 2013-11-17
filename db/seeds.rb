ce24a4e46dc25edad58a197eec4956be6e1d3c0c
John Doe <adam.q.pruett@gmail.com>
2013-11-17 10:27:36 -0800

    added Fluffy as Steve's pet for homework

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,4 @@ jeff.addresses.create!(street: '55 S Maple Ave', city: 'Appleton', state: 'WV', 
 john = Person.create!(first_name: 'John', last_name: 'Anderson')
 john.addresses.create!(street: '431 Cherry St', city: 'Vernon', state: 'CA', zip: '32344')
 john.addresses.create!(street: '51341 Grant Ave', city: 'Vernon', state: 'CA', zip: '33344')
-john.pets.create!(name: 'Kobe',genus: 'Schnauzer', person_id: 3)
+john.pets.create!(name: 'Kobe', genus: 'Schnauzer', person_id: 3)
