fb924a8f01813090d7c88362dc874616ef8e4689
John Doe <adam.q.pruett@gmail.com>
2013-11-17 11:07:59 -0800

    Added gender to People

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ steve.pets.create!(name: 'Joe', genus: 'Doberman', person_id: 1)
 jeff = Person.create!(first_name: 'Jeff', last_name: 'Matthews')
 jeff.addresses.create!(street: '55 S Maple Ave', city: 'Appleton', state: 'WV', zip: '32144')
 
-john = Person.create!(first_name: 'John', last_name: 'Anderson')
+john = Person.create!(first_name: 'John', last_name: 'Anderson', gender: 'male')
 john.addresses.create!(street: '431 Cherry St', city: 'Vernon', state: 'CA', zip: '32344')
 john.addresses.create!(street: '51341 Grant Ave', city: 'Vernon', state: 'CA', zip: '33344')
 john.pets.create!(name: 'Kobe', genus: 'Schnauzer', person_id: 3)
